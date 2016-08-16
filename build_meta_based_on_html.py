b9e5c2cb8d92de915499ce78bb95bfa32625e8a6
Steve <stg7@gmx.de>
2016-08-17 00:48:28 +0200

    add abstract extraction step

--- build_meta_based_on_html.py
+++ build_meta_based_on_html.py
@@ -2,35 +2,60 @@
 import os
 import sys
 import json
+import re
+import string
 from multiprocessing import Pool
 import multiprocessing
 
 
-def do_it(x):
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr,'', raw_html)
+    from html import unescape
+
+    tmp = unescape(cleantext.replace("\n", " "))
+
+    filtered_string = ""
+    for x in tmp:
+        if x in string.printable:
+            filtered_string += x
+
+    return filtered_string.strip()
+
+def extract_meta(x):
     f = open(x, "r", errors='ignore')
     title = ""
+    abstract = ""
+    abstract_start = False
     for l in f:
         if "<title>" in l:
             title = l.replace("<title>", "").replace("</title>", "").strip()
+        if "ABSTRACT</A>" in l:
+            abstract_start = True
+        if abstract_start and "AUTHORS</SPAN>" in l:
+            abstract_start = False
+        if abstract_start:
+            abstract += l
+
     f.close()
     newfile = x.replace("html/", "meta/").replace(".html", ".json")
     newpath = newfile.replace(os.path.basename(newfile), "")
     os.system("mkdir -p {p}".format(p=newpath))
     f = open(newfile, "w")
-    meta = {"title": title}
+    meta = {"title": title, "abstract": cleanhtml(abstract)}
     f.write(json.dumps(meta))
     f.close()
     print(title)
 
 def main(params):
     os.system("mkdir -p meta")
-    os.system("find html -name '*.html' > infiles")
+    os.system("find -L html -name '*.html' > infiles")
     infiles = [x.strip() for x in open("infiles", "r").readlines()]
 
     cpu_count = multiprocessing.cpu_count()
     pool = Pool(processes=cpu_count)
 
-    pool.map(do_it, infiles)
+    pool.map(extract_meta, infiles)
 
 
 if __name__ == "__main__":
