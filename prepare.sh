ba22af0175e955dfbc533964fcdb930feb9c9683
Steve <stg7@gmx.de>
2016-09-23 20:17:04 +0200

    update prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd dataset
-wget "http://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.apache.org/dist/lucene/solr/6.1.0/solr-6.1.0.tgz"
+wget -c "http://mirror2.shellbot.com/apache/lucene/solr/6.2.1/solr-6.2.1.tgz"
+tar -xvzf solr-6.2.1.tgz
 cd ..
